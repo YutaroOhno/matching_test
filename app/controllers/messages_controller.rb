593cbeac4f5968bd59840bc6631d5828b289eb83
大野佑太郎 <yutaro@oonoyuutarounoMacBook-puro.local>
2018-01-13 02:53:36 +0900

    align indent

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,21 +1,21 @@
 class MessagesController < ApplicationController
-	def show
-	  @user = User.find(params[:id])
-	  @your_messages = @user.messages.order("created_at DESC")
-	  @message = Message.new
-	  @my_messages = current_user.messages
-	  msg = @your_messages + @my_messages
-	  @messages = msg.sort{|a,b| a["created_at"] <=> b["created_at"]}
-	  # ここら辺のロジック汚いからなんとかしたい
+  def show
+    @user = User.find(params[:id])
+    @your_messages = @user.messages.order("created_at DESC")
+    @message = Message.new
+    @my_messages = current_user.messages
+    msg = @your_messages + @my_messages
+    @messages = msg.sort{|a,b| a["created_at"] <=> b["created_at"]}
+    # ここら辺のロジック汚いからなんとかしたい
 
     respond_to do |format|
-	    format.html
-		  format.json
-	  end
-	end
+      format.html
+      format.json
+    end
+  end
 
-	def create
-	  @message = current_user.messages.new(create_params)
+  def create
+    @message = current_user.messages.new(create_params)
     if @message.save
       respond_to do |format|
         format.json
@@ -23,11 +23,11 @@ class MessagesController < ApplicationController
     else
       render :show
     end
-	end
+  end
 
-	private
+  private
 
-	def create_params
-		params.require(:message).permit(:body)
-	end
+  def create_params
+    params.require(:message).permit(:body)
+  end
 end
